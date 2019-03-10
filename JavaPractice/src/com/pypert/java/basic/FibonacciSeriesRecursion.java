e43c87dae46983094bc23dc8d56c36bd869a711f
Kim S. Sunga <sungakim816@gmail.com>
2019-03-11 07:08:03 +0800

    Improved Fibonacci Recursion

--- JavaPractice/src/com/pypert/java/basic/FibonacciSeriesRecursion.java
+++ JavaPractice/src/com/pypert/java/basic/FibonacciSeriesRecursion.java
@@ -3,6 +3,8 @@
  */
 package com.pypert.java.basic;
 
+import java.util.Scanner;
+
 /**
  * @author Sunga
  *
@@ -13,19 +15,28 @@ public class FibonacciSeriesRecursion {
 	 * @param args
 	 */
 
-	public static int fibonacciRecurseSeries(int n) {
+	public static int fibonacciRecurseSeries(int n, int[] mem) {
 		if (n == 0 || n == 1) {
 			return n;
+		} else if (mem[n] != 0) {
+			return mem[n];
 		} else {
-			return fibonacciRecurseSeries(n - 1) + fibonacciRecurseSeries(n - 2);
+			mem[n] = fibonacciRecurseSeries(n - 1, mem) + fibonacciRecurseSeries(n - 2, mem);
+			return mem[n];
 		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < 10; i++) {
-			System.out.print(fibonacciRecurseSeries(i) + " ");
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter Number of Terms: ");
+		int terms = in.nextInt();
+		int[] memo = new int[terms];
+
+		for (int i = 0; i < terms; i++) {
+			System.out.print(fibonacciRecurseSeries(i, memo) + " ");
 		}
+		in.close();
 	}
 
 }
